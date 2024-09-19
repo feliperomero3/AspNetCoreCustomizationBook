23523731f5f47e62baa7e63c1cfeae8063a38c03
Felipe Romero <feliperomero3@outlook.com>
2024-09-18 22:33:02 -0600

    Enable NLog as logging provider.

--- src/01_LoggingSample/Program.cs
+++ src/01_LoggingSample/Program.cs
@@ -1,4 +1,5 @@
 using LoggingSample;
+using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ var config = new ColoredConsoleLoggerConfiguration
 };
 
 builder.Logging.AddProvider(new ColoredConsoleLoggerProvider(config));
+
+builder.WebHost.UseNLog();
 
 var app = builder.Build();
 
