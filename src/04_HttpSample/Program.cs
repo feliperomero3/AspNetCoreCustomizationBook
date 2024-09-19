4d89c9a1a1e374e1836473735c0e76ae2c1b02cf
Felipe Romero <feliperomero3@outlook.com>
2024-09-19 16:02:38 -0600

    Add Kestrel setup.

--- src/04_HttpSample/Program.cs
+++ src/04_HttpSample/Program.cs
@@ -1,4 +1,16 @@
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel(options =>
+{
+    options.Listen(IPAddress.Loopback, 5000);
+    options.Listen(IPAddress.Loopback, 5001, listenOptions =>
+    {
+        listenOptions.UseHttps("certificate.pfx", "password");
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
